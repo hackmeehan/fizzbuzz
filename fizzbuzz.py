d87e86b25df65c9152510072a18235059858b94d
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-09-18 12:33:48 -0400

    Updated from Brython Server: 9/18/2018 12:33:48 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -50,7 +50,9 @@ b = int(input("For multiples of what number shall we print 'Fizz'? "))
 c = int(input("For multiples of what number shall we print 'Buzz'? "))
 
 for i in range(1, a+1):
-    if i%b==0:
+    if i%(b*c)==0:
+        print('FizzBuzz')
+    elif i%b==0:
         print('Fizz')
     elif i%c==0:
         print('Buzz')
